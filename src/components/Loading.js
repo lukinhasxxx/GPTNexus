2299e9fb71496211dea35ec37a40eba4360c0cbb
joao01sb <bonfimnew@gmail.com>
2024-10-13 21:57:01 -0300

    feat: adiciona componente loading

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loading.css';
+
+const Loading = () => {
+    return (
+        <div className="loading-indicator">
+            <p>Carregando...</p>
+        </div>
+    );
+};
+
+export default Loading;
