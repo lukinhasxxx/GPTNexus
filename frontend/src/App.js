7155bca9b7ea389e1d195ee22e07b60e58b78051
Lucas da Silva <lucas.edi@outlook.com>
2024-11-11 00:57:35 -0300

    feat(theme): edita tema original e aprimora tema azul

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import './themes/originalTheme.css';
 import './themes/neonTheme.css'; //temas neon dark etc que serao implementados
-import './themes/blueTheme.css';
+import './themes/lightSkyTheme.css';
 import './themes/darkTheme.css';
 import './themes/8bitTheme.css';
 import ChatApp from "./ChatApp";
@@ -18,8 +18,8 @@ function ThemeButtons() {
             <button className="theme-button neon" onClick={() => toggleTheme('neon')}>
                 Tema Neon
             </button>
-            <button className="theme-button blue" onClick={() => toggleTheme('blue')}>
-                Tema Teste
+            <button className="theme-button blue" onClick={() => toggleTheme('lightsky')}>
+                Tema Light Sky
             </button>
             <button className="theme-button dark" onClick={() => toggleTheme('dark')}>
                 Tema Escuro
